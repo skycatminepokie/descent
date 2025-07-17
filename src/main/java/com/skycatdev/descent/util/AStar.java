d37ae7f8d9ec8f37028af6175d4e96c35e9b7c49
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-07-16 20:33:45 -0600

    Clean up path traversal
    Went crazy on the streams, so I tidied it up. Fewer collectors.

--- src/main/java/com/skycatdev/descent/util/AStar.java
+++ src/main/java/com/skycatdev/descent/util/AStar.java
@@ -3,7 +3,7 @@ package com.skycatdev.descent.util;
 import com.mojang.datafixers.util.Pair;
 import com.skycatdev.descent.DungeonPiece;
 import net.minecraft.util.math.BlockPos;
-import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.map_templates.BlockBounds;
 
@@ -17,8 +17,7 @@ import java.util.stream.Stream;
  * A (mostly) A* pathfinding algorithm. Allows for staircases that take up multiple cells.
  */
 public class AStar {
-    @Contract(mutates = "param1")
-    protected static Collection<DungeonPiece> findPath(Collection<DungeonPiece> paths,
+    protected static List<Node> findPath(Collection<DungeonPiece> paths,
                                                        Collection<BlockBounds> dungeon, // Everywhere we can't intersect - both paths and rooms
                                                        DungeonPiece.Opening start,
                                                        DungeonPiece.Opening end,
@@ -31,12 +30,12 @@ public class AStar {
 
         // Preconditions
         if (start.equals(end) || dist == 0) {
-            return paths; // TODO: Logging
+            return new ArrayList<>(); // TODO: Logging
         }
 
         if (dist == 1) {
             if (start.direction().getOpposite().equals(end.direction())) {
-                return paths;
+                return new ArrayList<>();
             }
             throw new NoSolutionException(); // TODO: Logging
         }
@@ -50,10 +49,10 @@ public class AStar {
             Node parent = open.stream()
                     .min(Comparator.comparingDouble(Node::pathLength))
                     .orElseThrow(() -> new RuntimeException("Open list was not empty, but its stream was?"));
-            open.remove(parent); // TODO: This might be slow
+            open.remove(parent); // TODO: This might be slow. Use open/closed sets instead of lists?
 
             // Generate successors
-            pieces.stream() // TODO: Parallelize?
+            var nodesByOpening = pieces.stream() // TODO: Parallelize?
                     .flatMap(piece -> piece.matchedWith(parent.opening())) // Find candidate openings
                     .filter(candidate -> dungeon.stream() // Find valid openings (don't intersect things)
                             .noneMatch(bounds -> bounds.intersects(candidate.piece().bounds()))) // TODO: Verify that this blocks all intersections
@@ -62,34 +61,59 @@ public class AStar {
                     .filter(node -> Stream.concat(open.stream(), closed.stream()) // Only keep faster ones
                             .noneMatch(other -> other.pathLength() < node.pathLength() && other.opening().equals(node.opening()))) // There are none at the same position with faster paths. TODO More random: if path lengths equal and openings equal, randomly choose whether to keep
                     // Make sure that two nodes with same opening but different lengths don't both end up on the open list
-                    .collect(Collectors.groupingBy(Node::opening)) // Map of opening to a list of nodes that have that opening
-                    .values().stream() // List of nodes with the same openings
-                    .flatMap(overlappedNodes -> overlappedNodes.stream()
-                            .collect(Collectors.groupingBy(Node::pathLength)) // Map of path length to list of overlapped nodes
-                            .entrySet().stream()
-                            .min(Comparator.comparingInt(Map.Entry::getKey)).orElseThrow().getValue().stream() // A list of overlapped nodes that have the lowest path length
-                    )
-                    .forEach(open::add);
+                    .collect(Collectors.groupingBy(Node::opening)); // Map of opening to a list of nodes that have that opening
+            
+            // Openings and nodes that have that opening
+            for (Map.Entry<DungeonPiece.Opening, List<Node>> entry : nodesByOpening.entrySet()) {
+                List<Node> nodes = entry.getValue();
+                
+                // Get only the fastest ones for this opening
+                List<Node> fastestNodes = getFastest(nodes);
 
+                if (entry.getKey().isConnected(end)) { // We've found node(s) next to the end!
+                    var finishingNode = fastestNodes.get(random.nextInt(fastestNodes.size())); // Choose a random one
+                    return finishingNode.path();
+                }
+                
+                open.addAll(fastestNodes);
+            }
+                
             closed.add(parent);
         }
-
-
-
-
-        return paths;
+        throw new NoSolutionException(); // TODO: Logging
     }
 
+    private static @NotNull List<Node> getFastest(List<Node> nodes) {
+        List<Node> fastestNodes = new LinkedList<>();
+        int fastest = Integer.MAX_VALUE;
+        for (Node node : nodes) {
+            if (node.pathLength() < fastest) {
+                fastest = node.pathLength();
+                fastestNodes.clear();
+                fastestNodes.add(node);
+            } else if (node.pathLength() == fastest) {
+                fastestNodes.add(node);
+            }
+        }
+        return fastestNodes;
+    }
+
+    /**
+     *
+     * @param prev The node to start searching from.
+     * @param placed The placed pieces to search for connections through.
+     * @param collector What to call when we've found a path through the placed pieces.
+     * @param endCenter The center of the ending opening (used for distance estimation)
+     */
     protected static void traverseAlreadyPlaced(Node prev, Collection<DungeonPiece> placed, Consumer<Node> collector, BlockPos endCenter) {
         // Try finding an already-placed piece that has a matching opening
         // If there is one, recurse. The next Nodes we make will have the root Node's piece
         // This signifies that we have access to those Nodes only if the root piece is placed.
 
-
         for (DungeonPiece piece : placed) {
             DungeonPiece.@Nullable Opening connected = piece.getConnected(prev.opening());
             if (connected != null) {
-                // Found a place for a new Node!
+                // Found a place for a new Node! (piece that has a connected opening)
 
                 Collection<DungeonPiece> piecesLeft = new LinkedList<>(placed);
                 piecesLeft.remove(piece); // Don't check this one - we don't want a loop, and we already handle this one
@@ -105,6 +129,7 @@ public class AStar {
                 }
                 return;
             }
+            // Nothing connected, try next
         }
 
         // We can't find any already-placed pieces that match, so this is a leaf.
@@ -130,6 +155,10 @@ public class AStar {
                                                     Collection<DungeonPiece> pieces,
                                                     int cellSize) {
         // TODO
+        Collection<DungeonPiece> paths = new LinkedList<>();
+        for (Pair<BlockBounds, BlockBounds> connection : toConnect) {
+            findPath(paths, )
+        }
         return null;
     }
 
@@ -143,6 +172,20 @@ public class AStar {
 
         public @Nullable DungeonPiece getPiece() {
             return piece;
+        }
+
+        /**
+         * 
+         * @return A list containing this Node, its parent (if it has one), that parent's parent (if it has one), and so on.
+         */
+        public List<Node> path() {
+            List<Node> ancestors = new LinkedList<>();
+            @Nullable Node current = this;
+            while (current != null) {
+                ancestors.add(current);
+                current = current.parent();
+            }
+            return ancestors;
         }
 
         /**
